6f22ed612a7256f4acca261565756a8a57bceaf2
AlejandroSanmiguel22 <alejandrosanmiguel0222@gmail.com>
2024-05-13 09:06:06 -0500

    Fix filtro controller

--- src/main/java/com/unibague/CitasMedicas/repository/CitaGeneralRepository.java
+++ src/main/java/com/unibague/CitasMedicas/repository/CitaGeneralRepository.java
@@ -2,6 +2,7 @@ package com.unibague.CitasMedicas.repository;
 
 import com.unibague.CitasMedicas.model.CitaGeneral;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 @Repository
 public interface CitaGeneralRepository extends JpaRepository<CitaGeneral, String> {
     CitaGeneral findByNumeroIdentificacion(String numeroIdentificacion);
+    @Query("SELECT c FROM CitaGeneral c WHERE c.nombre = :nombre AND c.tipo = :tipo AND c.costo BETWEEN :costoMinimo AND :costoMaximo AND c.id = :id")
     List<CitaGeneral> findByNombreTipoCostoId(String id, String nombre, String tipo, Double costoMinimo, Double costoMaximo);
 }
 
