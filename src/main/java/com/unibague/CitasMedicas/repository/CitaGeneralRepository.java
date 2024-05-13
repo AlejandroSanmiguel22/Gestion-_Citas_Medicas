65babb5ad8c668cb9e94fe749397a8ecb9d15b9c
AlejandroSanmiguel22 <alejandrosanmiguel0222@gmail.com>
2024-05-13 09:12:46 -0500

    Fix

--- src/main/java/com/unibague/CitasMedicas/repository/CitaGeneralRepository.java
+++ src/main/java/com/unibague/CitasMedicas/repository/CitaGeneralRepository.java
@@ -10,7 +10,8 @@ import java.util.List;
 @Repository
 public interface CitaGeneralRepository extends JpaRepository<CitaGeneral, String> {
     CitaGeneral findByNumeroIdentificacion(String numeroIdentificacion);
-    @Query("SELECT c FROM CitaGeneral c WHERE c.nombre = :nombre AND c.tipo = :tipo AND c.costo BETWEEN :costoMinimo AND :costoMaximo AND c.id = :id")
-    List<CitaGeneral> findByNombreTipoCostoId(String id, String nombre, String tipo, Double costoMinimo, Double costoMaximo);
+    @Query("SELECT c FROM CitaGeneral c WHERE c.nombrePaciente = :nombre AND c.tipoCita = :tipo AND c.costo BETWEEN :costoMinimo AND :costoMaximo AND c.numeroIdentificacion = :id")
+    List<CitaGeneral> findByNombreTipoCostoId(String nombre, String tipo, Double costoMinimo, Double costoMaximo, String id);
+
 }
 
